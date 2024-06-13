eb838e18e3c7de174b633f3474c13100a9261710
Gonzalo Santillan Plasencia <santillangon3@gmail.com>
2024-06-13 10:54:25 -0300

    Final del TP

--- Empleado.cs
+++ Empleado.cs
@@ -1,7 +1,7 @@
 
 namespace EspacioEmpleado;
 
-enum Cargo
+enum PosiblesCargos
 {
     Auxiliar,
     Administrativo,
@@ -11,6 +11,7 @@ enum Cargo
 }
 class Empleado
 {
+
     private string nombre;
 
     public string Nombre{
@@ -29,14 +30,64 @@ class Empleado
 
     private DateTime fechaNac; 
     public DateTime FechaNac { get => fechaNac; set => fechaNac = value; }
+
     private char estadoCivil;
-    public global::System.Char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
 
     private DateTime fechaIngreso;
     public DateTime FechaIngreso { get => fechaIngreso; set => fechaIngreso = value; }
+    
     private double sueldoBas;
-    public global::System.Double SueldoBas { get => sueldoBas; set => sueldoBas = value; }
-    internal Cargo Cargo { get => cargo; set => cargo = value; }
+    public double SueldoBas { get => sueldoBas; set => sueldoBas = value; }
 
-    private Cargo cargo;
+    private PosiblesCargos cargo;
+    public PosiblesCargos Cargo { get => cargo; set => cargo = value; }
+    
+    public int Antiguedad()
+    {
+        DateTime hoy=DateTime.Now;
+        int antiguedad = hoy.Year-fechaIngreso.Year;
+        return antiguedad;
+    }
+        
+    public int Edad()
+    {
+        DateTime hoy=DateTime.Now;
+        int edad = hoy.Year-fechaNac.Year;
+        return edad;
+    }
+    public int Jubilacion()
+    {
+        int jubilacion= 65- Edad();
+        return jubilacion;
+    }
+    public double Salario()
+    {
+        double salario;
+        if(Antiguedad()<20)
+        {
+            salario=SueldoBas+(SueldoBas*Antiguedad())/100;
+        }else
+        {
+            salario=SueldoBas*1.25;
+        }
+        if(Cargo==PosiblesCargos.Ingeniero||Cargo==PosiblesCargos.Especialista)
+        {
+            salario=salario*1.5;
+        }
+        if(EstadoCivil=='c'||EstadoCivil=='C')
+        {
+            salario=salario+150000;
+        }
+        return salario;
+    }
+
+    public void mostrarEmpleado()
+    {
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("\nNombre="+Nombre+"\nApellido:"+Apellido+"\nFecha de nacimiento:"+FechaNac+"\nEstado civil:"+EstadoCivil+"\nFecha de ingreso:"+FechaIngreso+"\nSueldo Basico:"+SueldoBas+"\nCargo:"+Cargo);
+
+        Console.ForegroundColor = ConsoleColor.DarkGreen;
+        Console.WriteLine("\nAntiguedad:"+Antiguedad()+" anios \nEdad:"+Edad()+" anios\nJubilacion:"+ Jubilacion()+" anios faltan para la jubulacion \nSalario:"+Salario());
+    }
 }
--- Program.cs
+++ Program.cs
@@ -1,68 +1,125 @@
 ﻿using EspacioCalculadora;
+using EspacioEmpleado;
 
 //----- Ejericio 1 -----
 
-    string? eleccion="10";  //El ? es para decir que es nulleable 
-    string? numS="";
-    double num=0;
+    // string? eleccion="10";  //El ? es para decir que es nulleable 
+    // string? numS="";
+    // double num=0;
 
 
-    Calculadora calc= new Calculadora();
+    // Calculadora calc= new Calculadora();
 
-    while(eleccion!="0")
-    {   
-        while(eleccion!="0"&&eleccion!="1"&&eleccion!="2"&&eleccion!="3"&&eleccion!="4"&&eleccion!="5")
-        {
-            Console.WriteLine("\nElija que operacion desea realizar:\n1-Sumar \n2-Restar \n3-Multiplicar \n4-Dividir\n5-Refresacar\n0-Salir");
-            eleccion=Console.ReadLine();
-            if(eleccion!="0" && eleccion!="5")
-            {
-                Console.WriteLine("\nIngrese el numero para operar:");
-                numS=Console.ReadLine();
-            } 
-        }
+    // while(eleccion!="0")
+    // {   
+    //     while(eleccion!="0"&&eleccion!="1"&&eleccion!="2"&&eleccion!="3"&&eleccion!="4"&&eleccion!="5")
+    //     {
+    //         Console.WriteLine("\nElija que operacion desea realizar:\n1-Sumar \n2-Restar \n3-Multiplicar \n4-Dividir\n5-Refresacar\n0-Salir");
+    //         eleccion=Console.ReadLine();
+    //         if(eleccion!="0" && eleccion!="5")
+    //         {
+    //             Console.WriteLine("\nIngrese el numero para operar:");
+    //             numS=Console.ReadLine();
+    //         } 
+    //     }
 
-        if(double.TryParse(numS, out num))
-        {
-            switch (eleccion)
-            {
-                case "1":
-                    calc.Sumar(num);
-                    break;
+    //     if(double.TryParse(numS, out num))
+    //     {
+    //         switch (eleccion)
+    //         {
+    //             case "1":
+    //                 calc.Sumar(num);
+    //                 break;
 
-                case "2":
-                    calc.Restar(num);
-                    break;
+    //             case "2":
+    //                 calc.Restar(num);
+    //                 break;
 
-                case "3":
-                    calc.Multiplicar(num);
-                    break;
+    //             case "3":
+    //                 calc.Multiplicar(num);
+    //                 break;
 
-                case "4":
-                    if(num!=0)
-                    {
-                        calc.Dividir(num);
-                    }else
-                    {
-                        Console.WriteLine("No se puede divir en 0");
-                    }
+    //             case "4":
+    //                 if(num!=0)
+    //                 {
+    //                     calc.Dividir(num);
+    //                 }else
+    //                 {
+    //                     Console.WriteLine("No se puede divir en 0");
+    //                 }
                         
-                    break;
+    //                 break;
 
-                case "5":
-                    calc.Limpiar();
-                    break;
-            }
+    //             case "5":
+    //                 calc.Limpiar();
+    //                 break;
+    //         }
 
-            Console.WriteLine("\nEl resultado es:"+ calc.Resultado());
-        }
-        if(eleccion!="0")
-        {
-            eleccion="A";
-        }
-    }
+    //         Console.WriteLine("\nEl resultado es:"+ calc.Resultado());
+    //     }
+    //     if(eleccion!="0")
+    //     {
+    //         eleccion="A";
+    //     }
+    // }
 
 
 //----- Ejercicio 2 -----
 
-Empleado emp =new Empleado()
+
+
+double montoTotal=0;
+int jubilacionMenor = 100;
+int iJub=0;
+int i=0;
+Empleado[] empleados =new Empleado[3];
+
+empleados[0]= new Empleado();
+empleados[0].Apellido="Perez";
+empleados[0].Nombre="Lucio";
+empleados[0].FechaNac=new DateTime(1995,5,15);
+empleados[0].EstadoCivil='S';
+empleados[0].FechaIngreso=new DateTime(2021,4,5);
+empleados[0].SueldoBas=50000;
+empleados[0].Cargo=PosiblesCargos.Auxiliar;
+
+empleados[1]= new Empleado();
+empleados[1].Apellido="Rodriguez";
+empleados[1].Nombre="Maria";
+empleados[1].FechaNac=new DateTime(1968,3,20);
+empleados[1].EstadoCivil='C';
+empleados[1].FechaIngreso=new DateTime(2010,9,15);
+empleados[1].SueldoBas=50000;
+empleados[1].Cargo=PosiblesCargos.Ingeniero;
+
+empleados[2]= new Empleado();
+empleados[2].Apellido="Martines";
+empleados[2].Nombre="Rodrigo";
+empleados[2].FechaNac=new DateTime(1980,10,1);
+empleados[2].EstadoCivil='S';
+empleados[2].FechaIngreso=new DateTime(2016,6,10);
+empleados[2].SueldoBas=50000;
+empleados[2].Cargo=PosiblesCargos.Especialista;
+
+foreach (var emp in empleados)
+{
+    
+    emp.mostrarEmpleado();
+
+    montoTotal=montoTotal+emp.Salario();
+
+    if(emp.Jubilacion()<jubilacionMenor)
+    {
+        jubilacionMenor=emp.Jubilacion();
+        iJub=i;
+    }
+    i++;
+}
+
+Console.ForegroundColor = ConsoleColor.DarkBlue;
+Console.WriteLine("\nEl monto total a pagar a los empleado es de:"+montoTotal);
+
+Console.WriteLine("\nEl empleado mas proximo a la jubilacion es:");
+empleados[iJub].mostrarEmpleado();
+
+
